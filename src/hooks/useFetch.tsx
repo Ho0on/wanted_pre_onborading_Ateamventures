1effcfac5a6b5af771e2af901dce4dc350ac1c94
SangHoon <joa3668@gmail.com>
2022-02-08 15:19:34 +0900

    Add: useFetch hook 추가

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -1,5 +1,27 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
-const useFetch = () => {};
+const useFetch = (url: string) => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState<unknown>();
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const response = await (await fetch(url)).json();
+        setData(response);
+        setIsLoading(false);
+      } catch (error) {
+        setError(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return { data, error, isLoading };
+};
 
 export default useFetch;
--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -3,10 +3,13 @@ import Nav from 'components/Nav/Nav';
 import * as S from './Main.style';
 import FilterContent from 'components/FilterContent/FilterContent';
 import { MATERIAL, PROCESS_METHOD } from 'contants';
+import useFetch from 'hooks/useFetch';
 
 function Main() {
   const [isToggled, setIsToggled] = useState(false);
   const [isActive, setIsActive] = useState(false);
+
+  const { data, error, isLoading } = useFetch('http://localhost:4000/requests');
 
   const handleToggle = () => {
     setIsToggled(!isToggled);
@@ -19,42 +22,46 @@ function Main() {
   return (
     <>
       <Nav />
-      <S.Container>
-        <S.HeaderTitle>들어온 요청</S.HeaderTitle>
-        <S.HeaderDesc>파트너님에게 딱 맞는 요청서를 찾아보세요.</S.HeaderDesc>
-        <S.FilteringContainer>
-          <S.FilteringBoxWrapper>
-            <FilterContent
-              title="가공 방식"
-              filterData={PROCESS_METHOD}
-              isActive={isActive}
-              setIsActive={setIsActive}
-            />
-            <FilterContent
-              title="재료"
-              filterData={MATERIAL}
-              isActive={isActive}
-              setIsActive={setIsActive}
-            />
-            <S.ResetWrapper onClick={handleResetActive}>
-              <S.ResetIcon src="/images/refresh_icon.png" />
-              <S.ResetText>필터링 리셋</S.ResetText>
-            </S.ResetWrapper>
-          </S.FilteringBoxWrapper>
-          <S.ToggleWrapper>
-            <S.InputWrapper>
-              <S.InputCheck
-                type="checkbox"
-                checked={isToggled}
-                onChange={handleToggle}
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <S.Container>
+          <S.HeaderTitle>들어온 요청</S.HeaderTitle>
+          <S.HeaderDesc>파트너님에게 딱 맞는 요청서를 찾아보세요.</S.HeaderDesc>
+          <S.FilteringContainer>
+            <S.FilteringBoxWrapper>
+              <FilterContent
+                title="가공 방식"
+                filterData={PROCESS_METHOD}
+                isActive={isActive}
+                setIsActive={setIsActive}
               />
-              <S.Switch />
-            </S.InputWrapper>
-            <S.ToggleText>상담 중인 요청만 보기</S.ToggleText>
-          </S.ToggleWrapper>
-        </S.FilteringContainer>
-        <S.ReqWrapper>조건에 맞는 견적 요청이 없습니다.</S.ReqWrapper>
-      </S.Container>
+              <FilterContent
+                title="재료"
+                filterData={MATERIAL}
+                isActive={isActive}
+                setIsActive={setIsActive}
+              />
+              <S.ResetWrapper onClick={handleResetActive}>
+                <S.ResetIcon src="/images/refresh_icon.png" />
+                <S.ResetText>필터링 리셋</S.ResetText>
+              </S.ResetWrapper>
+            </S.FilteringBoxWrapper>
+            <S.ToggleWrapper>
+              <S.InputWrapper>
+                <S.InputCheck
+                  type="checkbox"
+                  checked={isToggled}
+                  onChange={handleToggle}
+                />
+                <S.Switch />
+              </S.InputWrapper>
+              <S.ToggleText>상담 중인 요청만 보기</S.ToggleText>
+            </S.ToggleWrapper>
+          </S.FilteringContainer>
+          <S.ReqWrapper>조건에 맞는 견적 요청이 없습니다.</S.ReqWrapper>
+        </S.Container>
+      )}
     </>
   );
 }
