2ff28d60aa5839b81addf878518295605fc04eea
tohjbin <tohjbin@gmail.com>
2022-02-08 22:26:46 +0900

    WIP

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -48,6 +48,7 @@ function Main() {
       <S.Container>
         <S.HeaderTitle>들어온 요청</S.HeaderTitle>
         <S.HeaderDesc>파트너님에게 딱 맞는 요청서를 찾아보세요.</S.HeaderDesc>
+        메인브랜치ddd
         <S.FilteringContainer>
           <S.FilteringBoxWrapper>
             <FilterContent
